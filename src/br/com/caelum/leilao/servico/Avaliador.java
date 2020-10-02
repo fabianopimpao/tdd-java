21b0ba1aa63dbbc421f9d07a2855b3318c6d8aee
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-10-02 06:56:18 -0300

    Testando o que é necessário em uma aplicação

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,5 +1,10 @@
 package br.com.caelum.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
@@ -7,7 +12,8 @@ public class Avaliador {
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
-    private double valorMedio = 0;    
+    private double valorMedio = 0;
+	private List<Lance> maiores = new ArrayList<>();    
     
     public void avalia(Leilao leilao) {
     	double soma = 0;
@@ -28,6 +34,23 @@ public class Avaliador {
         }
         
         valorMedio = soma / leilao.getLances().size();
+        maiores.addAll(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+			@Override
+			public int compare(Lance o1, Lance o2) {
+				if (o1.getValor() < o2.getValor()) {
+					return 1;
+				}
+				
+				if (o1.getValor() > o2.getValor()) {
+					return -1;
+				}
+				
+				return 0;
+			}
+		});
+        
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
     }
 
     public double getMaiorLance() {
@@ -40,5 +63,9 @@ public class Avaliador {
     
     public double getValorMedio() {
 		return valorMedio;
+	}
+    
+    public List<Lance> getTresMaiores() {
+		return maiores;
 	}
 }
